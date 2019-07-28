bbcadf5d2426cb46f64ea9e420f5c08632b9ca17
Midzuk <ucdmzk@icloud.com>
2019-07-29 02:28:14 +0900

    修正

---
+++ R/sample.R
@@ -0,0 +1,8 @@
+library(R6)
+
+# user
+User <- R6Class("User",
+                public = list(cost_limit = NULL,
+                              time_value = NULL,
+                              
+                              ))
