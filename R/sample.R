ab7d1a08f2b1bf9f91df2ba54cddd3470b97b7b0
Midzuk <ucdmzk@icloud.com>
2019-08-01 00:35:08 +0900

    修正

--- R/sample.R
+++ R/sample.R
@@ -12,112 +12,138 @@ Location <- R6Class("Location",
                                     self$lat <- lat
                                   },
                                   distance = function(location) {
-                                    distance <- function(lon1,
-                                                         lat1,
-                                                         lon2,
-                                                         lat2) {
+                                    
+                                    f <- function(lon1,
+                                                  lat1,
+                                                  lon2,
+                                                  lat2) {
                                       #暫定
                                       return(sqrt((lon2 - lon1) ^ 2 + (lat2 - lat1) ^ 2))
                                     }
                                     
-                                    return(distance(self$lon,
-                                                    self$lat,
-                                                    location$lon,
-                                                    location$lat))
+                                    return(f(self$lon,
+                                             self$lat,
+                                             location$lon,
+                                             location$lat))
                                   }))
 
 # mesh
-Mesh <- R6Class("Mesh",
-                public = list(location = NULL,
-                              user_df = NULL,
-                              users = NULL,
-                              
-                              initialize = function(location = NA,
-                                                    facs = NA,
-                                                    user_df = NA) {
-                                self$location <- location
-                                self$user_df <- user_df
-                                
-                                user_num <- rpois(user_df$facility_id %>% length(),
-                                                  lambda = user_df$user_avg)
-                                
-                                self$users <- list()
-                                for (i in user_df$facility_id) {
-                                  self$users[[i]] <- list()
-                                  
-                                  if (user_num[i] != 0) {
-                                    for (j in 1:user_num[i]) {
-                                      self$users[[i]][[j]] <- User$new(origin = location,
-                                                                       destination = facs[[i]])
-                                    }
-                                  }
-                                }
-                              }))
-
-# make meshs
-make_meshs <- function(mesh_df = NA,
-                       fac_df = NA,
-                       user_df = NA) {
-  facs <- list()
-  for (i in fac_df$facility_id) {
-    facs[[i]] <- Location$new(lon = lon[i],
-                              lat = lat[i])
-  }
-  
-  meshs <- list()
-  
-  lon <- mesh_df %>%
-    .$lon
-  lat <- mesh_df %>%
-    .$lat
-  
-  for (i in mesh_df$mesh_id) {
-    meshs[[i]] <- Mesh$new(location = Location$new(lon[i],
-                                                   lat[i]),
-                           facs = facs,
-                           user_df = user_df %>%
-                             filter(mesh_id == i) %>%
-                             select(-mesh_id))
-  }
-  
-  return(meshs)
-}
+# Mesh <- R6Class("Mesh",
+#                 inherit = Location,
+#                 public = list(lon = NULL,
+#                               lat = NULL,
+#                               
+#                               initialize = function(lon = NA,
+#                                                     lat = NA,
+#                                                     ...) { # 暫定
+#                                 super$initialize(lon = lon,
+#                                                  lat = lat)
+#                               },
+#                               
+#                               distance = function(location) {
+#                                 return(super$distance(location))
+#                               }))
 
 # user
 User <- R6Class("User",
-                public = list(origin = NULL,
-                              destination = NULL,
-                              
+                private = list(time_value = NULL,
+                               
+                               stay_time = NULL,
+                               
+                               passed_time = NULL),
+                
+                public = list(mesh = NULL,
+                              facility = NULL,
                               distance = NULL,
                               
-                              depart_time_home = NULL,
-                              stay_time = NULL,
-                              depart_time_fac = NULL,
-                              arrival_time = NULL,
+                              back = NULL,
                               
-                              generalized_cost = NULL, # 暫定的にかかる時間
+                              depart_time = NULL,
                               
-                              initialize = function(origin = NA, 
-                                                    destination = NA) {
-                                self$origin <- origin
-                                self$destination <- destination
+                              initialize = function(time_value = NA,
+                                                    mesh = NA, 
+                                                    facility = NA) {
+                                private$time_value <- time_value
                                 
+                                self$mesh <- mesh
+                                self$facility <- facility
                                 self$distance <- origin$distance(destination)
                                 
-                                self$depart_time_home <- runif(1) # 暫定
-                                self$stay_time <- runif(1) # 暫定
+                                self$back <- FALSE
                                 
+                                self$depart_time <- runif(1) # 暫定
+                                private$stay_time <- runif(1) # 暫定
+                                
+                                private$passed_time <- 0
+                                
+                              },
+                              
+                              pass = function(time) {
+                                private$passed_time <- private$passed_time + time
+                              },
+                              
+                              get_generalized_cost = function(fare = NA) {
+                                return(private$time_value * private$passed_time + fare)
                               }))
+
+# self$location <- location
+# self$user_df <- user_df
+# 
+# user_num <- rpois(user_df$facility_id %>% length(),
+#                   lambda = user_df$user_avg)
+# 
+# self$users <- list()
+# for (i in user_df$facility_id) {
+#   self$users[[i]] <- list()
+# 
+#   if (user_num[i] != 0) {
+#     for (j in 1:user_num[i]) {
+#       self$users[[i]][[j]] <- User$new(origin = location,
+#                                        destination = facs[[i]])
+#     }
+#   }
+# }
+
+# make meshs
+# make_meshs <- function(mesh_df = NA,
+#                        fac_df = NA,
+#                        user_df = NA) {
+#   facs <- list()
+#   for (i in fac_df$facility_id) {
+#     facs[[i]] <- Location$new(lon = lon[i],
+#                               lat = lat[i])
+#   }
+#   
+#   meshs <- list()
+#   
+#   lon <- mesh_df %>%
+#     .$lon
+#   lat <- mesh_df %>%
+#     .$lat
+#   
+#   for (i in mesh_df$mesh_id) {
+#     meshs[[i]] <- Mesh$new(location = Location$new(lon[i],
+#                                                    lat[i]),
+#                            facs = facs,
+#                            user_df = user_df %>%
+#                              filter(mesh_id == i) %>%
+#                              select(-mesh_id))
+#   }
+#   
+#   return(meshs)
+# }
+
 
 # car
 Car <- 
 
 # 暫定データ
+# facility
 fac_df <- tribble(
   ~facility_id, ~lon, ~lat,
   1, 5, 4,
   2, 3, 6,
-  3, 4 , 5,
+  3, 4, 5,
   4, 5, 6,
   5, 6, 6
 )
@@ -127,11 +153,15 @@ lon <- fac_df %>%
 lat <- fac_df %>%
   .$lat
 
-user_df <- tibble(mesh_id = 1:100,
-                   facility_id = list(1:5)) %>%
-  unnest() %>%
-  mutate(user_avg = 5)
+facs <- list()
 
+for (i in fac_df$facility_id) {
+  facs[[i]] <- Location$new(lon[i], lat[i])
+}
+
+
+
+# mesh
 mesh_df <- tibble(lon = 0:9,
                   lat = list(0:9)) %>%
   unnest() %>%
@@ -140,30 +170,28 @@ mesh_df <- tibble(lon = 0:9,
          lon,
          lat)
 
-meshs <- make_meshs(mesh_df,
-                    fac_df,
-                    user_df)
+lon <- mesh_df %>%
+  .$lon
+lat <- mesh_df %>%
+  .$lat
 
-users <- list()
+meshs <- list()
 
-for (i in 1:length(meshs)) {
-  for (j in 1:length(meshs[[i]])) {
-    if (length(meshs[[i]]$users[[j]]) != 0) {
-      for (k in 1:length(meshs[[i]]$users[[j]])) {
-        meshs[[]]
-      } 
-    }
-  }
-  users %<>%
-    append(users, meshs[[i]]$users)
+for (i in mesh_df$mesh_id) {
+  meshs[[i]] <- Location$new(lon[i], lat[i])
 }
 
-         # mesh = Mesh$new(location = Location$new(lon = lon,
-         #                                         lat = lat)))
-#   select(mesh_id,
-#          lon,
-#          lat)
 
 
-# rpois
+# user
+user_df <- tibble(mesh_id = 1:100,
+                   facility_id = list(1:5)) %>%
+  unnest() %>%
+  mutate(user_avg = 5)
+
+make_users <- function(user_df = NA,
+                       meshs = NA,
+                       facs = NA) {
+  
+}
 
